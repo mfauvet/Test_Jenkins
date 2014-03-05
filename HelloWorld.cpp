2c1fa70ae1f31994003b8a04377182197f527e5e
mfauvet <morgane.fauvet@mensiatech.com>
2014-03-05 17:29:33 +0100

    Test

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,12 +5,12 @@
 **/
 void CHelloWorld::CHelloWorld::display()
 {
+	int machin;
+	machin = 1;
 	if(this->truc==3)
 			std::cout<<"Hello";
 	else
 	{
-		int machin;
-		machin = 1;
 		machin = std::min(machin, 3);
 		machin;
 	}
