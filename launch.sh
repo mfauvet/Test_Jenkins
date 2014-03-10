99964cfa52ee8b1d105d9ced644f429d6612213a
mfauvet <morgane.fauvet@mensiatech.com>
2014-03-10 18:42:38 +0100

    Edit launch.sh to run tests without env variables set

--- launch.sh
+++ launch.sh
@@ -1,4 +1,3 @@
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DPYTHON_EXECUTABLE="C:/Python27/python.exe" -DGCOVR_PATH="C:/Python27/Scripts/gcovr"
-mingw32-make
-mingw32-make Test_coverage
-PAUSE
+C:/openvibe/dependencies/cmake/bin/cmake.exe -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DPYTHON_EXECUTABLE="C:/Python27/python.exe" -DGCOVR_PATH="C:/Python27/Scripts/gcovr"
+C:/MinGW/bin/mingw32-make.exe
+C:/MinGW/bin/mingw32-make Test_coverage
