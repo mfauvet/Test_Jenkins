7b7f94aec21186fbabf13c7a5c04e7970159ac90
mfauvet <morgane.fauvet@mensiatech.com>
2014-03-14 16:26:27 +0100

    Edit launch et CMakeLists

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 mkdir minGW-build
 cd minGW-build
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DPYTHON_EXECUTABLE="C:/Python27/python.exe" -DGCOVR_PATH="C:/Python27/Scripts/gcovr" -Dtest=ON
+cmake .. -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DPYTHON_EXECUTABLE="C:/Python27/python.exe" -DGCOVR_PATH="C:/Python27/Scripts/gcovr" -Dtest=ON -Dgcovr=ON
 C:/MinGW/bin/mingw32-make.exe
-C:/MinGW/bin/mingw32-make Test_coverage
-C:/MinGW/bin/mingw32-make Tests
+C:/MinGW/bin/mingw32-make.exe Test_coverage
+C:/MinGW/bin/mingw32-make.exe Tests
+read truc
