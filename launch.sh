7e002ed5872409c4355dfe333c6e1d59791d742c
mfauvet <morgane.fauvet@mensiatech.com>
2014-03-14 16:16:49 +0100

    use cmake instead of path

--- launch.sh
+++ launch.sh
@@ -1,4 +1,6 @@
-C:/openvibe/dependencies/cmake/bin/cmake.exe -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DPYTHON_EXECUTABLE="C:/Python27/python.exe" -DGCOVR_PATH="C:/Python27/Scripts/gcovr" -Dtest=ON
+mkdir minGW-build
+cd minGW-build
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DPYTHON_EXECUTABLE="C:/Python27/python.exe" -DGCOVR_PATH="C:/Python27/Scripts/gcovr" -Dtest=ON
 C:/MinGW/bin/mingw32-make.exe
 C:/MinGW/bin/mingw32-make Test_coverage
 C:/MinGW/bin/mingw32-make Tests
