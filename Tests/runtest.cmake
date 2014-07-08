ea025006d8ba09483c38413ffd3b5a904baafd7f
mfauvet <morgane.fauvet@mensiatech.com>
2014-07-08 16:12:23 +0200

    Test

--- Tests/runtest.cmake
+++ Tests/runtest.cmake
@@ -5,9 +5,14 @@ if(HAD_ERROR)
 endif()
 
 execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files
+<<<<<<< Updated upstream
     ${CMAKE_BINARY_DIR}/obtained.txt ${CMAKE_CURRENT_SOURCE_DIR}/Tests/expected.txt
     RESULT_VARIABLE DIFFERENT
 )
+=======
+    ${CMAKE_BINARY_DIR}/obtained.txt ${CMAKE_SOURCE_DIR}/Tests/expected.txt
+    RESULT_VARIABLE DIFFERENT)
+>>>>>>> Stashed changes
 if(DIFFERENT)
     #execute_process(COMMAND svn diff expected.txt)
 	message(FATAL_ERROR "Test failed - ${CMAKE_BINARY_DIR}/obtained.txt and ${HELLO_SOURCE_DIR}/expected.txt differ")
